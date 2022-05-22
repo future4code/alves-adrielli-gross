94570ce9fb8c572378e0801b765fb983f06c4a56
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-05-22 19:03:11 -0300

    alterações jogo

--- modulo1/projeto-blackjack/Blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/Blackjack/nossoJogo.js
@@ -1,23 +1,39 @@
 
 // BLACKJACK INICIO
-    console.log("Boas vindas ao jogo de Blackjack!")
+
+/*console.log("Boas vindas ao jogo de Blackjack!")
 if(confirm("Quer iniciar uma nova rodada?")) {
 } else {
 	console.log("O jogo acabou!")
 }
 
 // COMEÇO DA RODADA
-const carta = comprarCarta(); 
+const carta = comprarCarta(); // Usuário
 const segCarta = comprarCarta();
-console.log(`Você possuí esta carta: ${carta.texto} e ${segCarta.texto}.`) // Usuário
+console.log(`Você possuí esta carta: ${carta.texto} e ${segCarta.texto}.`) 
 let somaUser = carta.valor + segCarta.valor
 console.log(`Sua pontuação é de: ${somaUser}.`) 
 
-const cartaPc = comprarCarta();
+const cartaPc = comprarCarta(); // Computador
 const segPc = comprarCarta();
-console.log(`O computador possuí esta carta: ${cartaPc.texto} e ${segPc.texto}.`) // Computador
+console.log(`O computador possuí esta carta: ${cartaPc.texto} e ${segPc.texto}.`) 
 let somaPc = cartaPc.valor + segPc.valor
 console.log(`A pontuação do computador é: ${somaPc}.`)
 
+if(somaUser > somaPc){
+    console.log(`O usuário ganhou!`)
+}else if(somaUser == somaPc){
+    console.log(`Empate!`)
+}else if(somaPc > 21){
+    console.log(`O usuário ganhou!`)
+}else if(somaUser > 21){
+    console.log(`O computador ganhou!`)
+}else if(somaPc > 21 && somaUser > 21){
+    console.log(`A soma do usuário e do computador deram maior que 21, os dois perderam!`)
+}
+else {
+    console.log(`O computador ganhou!`)
+}
+*/
 
 
