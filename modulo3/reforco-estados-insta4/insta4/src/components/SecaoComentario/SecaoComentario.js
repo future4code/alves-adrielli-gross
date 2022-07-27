af2cd10c652a11b178e1066fb2392e9492adff52
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-07-27 17:37:58 -0300

    Reforço

---
+++ modulo3/reforco-estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,34 @@
+import React, {Component, useState} from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+
+export function SecaoComentario (props) {
+	const [ inputComentario, setInputComentario ] = useState("")
+
+	const onChangeComentario = () => {
+		setInputComentario()
+	}
+
+	
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				value={inputComentario}
+				onChange={onChangeComentario}
+			/>
+			<button onClick={props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+	
+}
+
+export default SecaoComentario;
