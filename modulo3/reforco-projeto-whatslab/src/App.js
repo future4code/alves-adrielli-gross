d287d18b3546ea66bb826688215eca38bcdbe5ac
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-07-31 20:39:55 -0300

    Finalização

--- modulo3/reforco-projeto-whatslab/src/App.js
+++ modulo3/reforco-projeto-whatslab/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Conversa from './Components/Conversa';
+import styled from 'styled-components';
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;  
+  height: 100vh;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Conversa>
+      </Conversa>
+    </Main>
   );
 }
 
