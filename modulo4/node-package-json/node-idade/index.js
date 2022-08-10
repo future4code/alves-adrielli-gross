07e9ce3756507ab536293d5de1fb29b626e72fad
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-08-09 21:56:54 -0300

    Desafio

--- modulo4/node-package-json/node-idade/index.js
+++ modulo4/node-package-json/node-idade/index.js
@@ -1,9 +1,18 @@
 // 1) a- process.argv[]
 
+// DESAFIO
+const azul = '\x1b[36m%s\x1b[0m'
+
+const valor = Number(process.argv[3])
+if (process.argv[2] === undefined) {
+    console.log("Você precisa digitar parâmetros")
+} else if (isNaN(valor.NaN)) {
+    console.log("Você precisa digitar dois parâmetros")
+} else {
 // 1) b-
-console.log("Olá, Adrielli! Você tem 25 anos.")
-console.log("Olá, ", process.argv[2], "! você tem ", process.argv[3], "anos.")
+console.log(azul, "Olá, Adrielli! Você tem 25 anos.")
+console.log("Olá, ", process.argv[2], "! você tem ", valor, "anos.")
 
 // 1) c-
-const valor = Number(process.argv[3])
-console.log("Olá,", process.argv[2], `! você tem ${valor} anos. Daqui 7 anos você terá`, valor + 7, ".")
+console.log(azul, "Olá,", process.argv[2], `! você tem ${valor} anos. Daqui 7 anos você terá`, valor + 7, ".")}
+
--- modulo4/node-package-json/node-tarefa/index.js
+++ modulo4/node-package-json/node-tarefa/index.js
@@ -1,8 +1,22 @@
 
 // 3)
-let lista = ["Comprar leite"]
-let novalista = [...lista, process.argv[2]]
+const fs = require('fs');
 
-lista = novalista
-console.table(lista)
+const tarefas = [
+    "Lavar a Louça",
+    "Comprar Leite"
+];
 
+const tarefa2 = process.argv[2]
+tarefas.push(tarefa2)
+
+
+const listaTarefas = JSON.stringify(tarefas)
+console.log(listaTarefas)
+console.log(__dirname)
+
+fs.writeFileSync(__dirname + "/save.json", listaTarefas)
+
+const itensSave = fs.readFileSync(__dirname + "/save.json")
+
+console.log(JSON.parse(itensSave))
