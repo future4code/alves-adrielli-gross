bde55abf9856265963d7753d1f95473dfdd53a02
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-08-09 21:54:42 -0300

    Alterações+desafio

--- modulo4/node-package-json/node-math/index.js
+++ modulo4/node-package-json/node-math/index.js
@@ -1,4 +1,5 @@
 // 2)
+const amarelo = '\x1b[33m%s\x1b[0m'
 switch(process.argv[2]){
 	case "add":
 		console.log(+process.argv[3] + (+process.argv[4]))
@@ -12,5 +13,7 @@ switch(process.argv[2]){
     case "div":
 		console.log(+process.argv[3] / (+process.argv[4]))
 		break;
+	default:
+		console.log(`${amarelo}`, 'Você precisa digitar:', 'add, sub, mult ou div antes dos parâmetros.')
     }
 
